c86c1d74c055809551309dd06e990389f5c15ca7
bogvoicescu <bog.voicescu@gmail.com>
2022-03-12 10:10:30 +0200

    app structure

---
+++ src/containers/Foot.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Foot extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Foot;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Foot from './containers/Foot';
+import Main from './containers/Main';
+import Nav from './containers/Nav';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Nav />
+    <Main />
+    <Foot />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
